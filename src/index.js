31831b1251abd0614e1473c81a95019857bc4a1b
Piotr Sobieski <psobieski4@gmail.com>
2022-07-02 17:14:50 +0200

    sass config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './scss/index.scss';
+import './scss/main.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
